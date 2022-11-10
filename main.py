80c151ff38e58962efc26143b1a38defee003cd7
Matt Millsaps <mattmillsaps.dev@gmail.com>
2022-11-09 20:36:37 -0500

    removed guide comment from final project

--- main.py
+++ main.py
@@ -73,7 +73,6 @@ def play_game():
     print(f"   Computer's final hand: {computer_cards} | Final Score: {computer_score}")
     print(compare(user_score, computer_score))
 
-    #Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
 while input("\nDo you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
     clear()
     play_game()
